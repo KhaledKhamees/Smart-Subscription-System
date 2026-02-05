96e99cd1e11476aa3ae147dbfb186f70099bc155
khaled <khaledkhameesabdelaleem@gmail.com>
2026-02-05 02:19:59 +0200

    Debug subscription endpoint

--- SubscriptionService/DTOs/CreateSubscriptionDTO.cs
+++ SubscriptionService/DTOs/CreateSubscriptionDTO.cs
@@ -6,8 +6,5 @@ namespace SubscriptionService.DTOs
     {
         public Guid UserId { get;  set; }
         public Guid PlanId { get;  set; }
-        public DateTime StartDate { get;  set; }
-        public DateTime? EndDate { get;  set; }
-        public DateTime NextBillingDate { get;  set; }
     }
 }

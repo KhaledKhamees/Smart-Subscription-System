5505aaf31be36d6b506e756ff0891e205c40fb09
khaled <khaledkhameesabdelaleem@gmail.com>
2026-02-13 23:42:31 +0200

    BillingService : Add Enitities

---
+++ BillingService/Entities/Enums/SubscriptionLifecycleStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace BillingService.Entities.Enums
+{
+    public enum SubscriptionLifecycleStatus
+    {
+        Trial = 0,
+        Active = 1,
+        GracePeriod = 2,
+        PastDue = 3,
+        Canceled = 4,
+        Expired = 5
+    }
+}
---
+++ BillingService/Entities/SubscriptionBilling.cs
@@ -0,0 +1,30 @@
+﻿using BillingService.Entities.Enums;
+
+namespace BillingService.Entities
+{
+    public class SubscriptionBilling
+    {
+        public Guid Id { get; set; }  // = SubscriptionId 
+        public Guid UserId { get; set; }
+        public Guid PlanId { get; set; }
+        public SubscriptionLifecycleStatus Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? TrialEndDate { get; set; }
+        public DateTime? CurrentPeriodStart { get; set; }
+        public DateTime? CurrentPeriodEnd { get; set; }
+        public DateTime? CanceledAt { get; set; }
+        public DateTime? GracePeriodEndDate { get; set; }
+
+        // Stripe references
+        public string? StripeCustomerId { get; set; }
+        public string? StripeSubscriptionId { get; set; }
+
+        // Failure tracking
+        public int ConsecutiveFailures { get; set; }
+        public DateTime? LastPaymentAttempt { get; set; }
+
+        // Navigation
+        public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    }
+}
